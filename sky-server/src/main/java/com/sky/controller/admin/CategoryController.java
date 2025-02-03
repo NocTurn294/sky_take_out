f67841e12390e7140bf0de9cdd07e26e799ad5ad
turnin <2944603600@qq.com>
2025-02-03 15:52:27 +0800

    分类管理模块的分类分页查询实现

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,4 +1,28 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+@GetMapping("/page")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        log.info("菜品目录分页查询：{}",categoryPageQueryDTO);
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+
+    }
 }
