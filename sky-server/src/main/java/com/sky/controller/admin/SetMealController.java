9f7dc0408b21d917cb3b4ad09077549ca56e2b33
turnin <2944603600@qq.com>
2025-02-07 19:04:58 +0800

    套餐分页查询

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -0,0 +1,27 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/setmeal")
+public class SetMealController {
+    @Autowired
+    private SetmealService setmealService;
+     @GetMapping("/page")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("菜品分页查询：{}",setmealPageQueryDTO);
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+}
