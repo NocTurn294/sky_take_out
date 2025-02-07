8735c8451a11040dc2e4a190d21580dede8b873f
turnin <2944603600@qq.com>
2025-02-07 22:04:47 +0800

    修改套餐启用状态

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -14,16 +14,21 @@ public interface SetmealService {
     void save(SetmealDTO setmealDTO);
 
     void deleteBatch(List<Long> ids);
+
     /**
      * 根据id查询套餐和关联的菜品数据
+     *
      * @param id
      * @return
-*/
-SetmealVO getByIdWithDish(Long id);
+     */
+    SetmealVO getByIdWithDish(Long id);
 
-/**
+    /**
      * 修改套餐
+     *
      * @param setmealDTO
-*/
-void update(SetmealDTO setmealDTO);
+     */
+    void update(SetmealDTO setmealDTO);
+
+    void startOrStop(Integer status, Long id);
 }
