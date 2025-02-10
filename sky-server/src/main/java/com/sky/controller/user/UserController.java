714737f8d6bbf16c491e97596cb715937bfaf70d
turnin <2944603600@qq.com>
2025-02-10 10:57:09 +0800

    微信小程序登录后查询菜品

--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -43,6 +43,6 @@ public class UserController {
                 .openid(user.getOpenid())
                 .token(token)
                .build();
-        return Result.success();
+        return Result.success(userLoginVO);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -174,4 +174,6 @@ public class DishServiceImpl implements DishService {
 
         return dishVOList;
     }
+
+
 }
